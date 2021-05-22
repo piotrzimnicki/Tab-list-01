3e87c081a1860c2cb2eec4b471c8ece0b1a0210a
Piotr Zimnicki <ziemniakit@gmail.com>
2021-05-22 15:58:59 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let navWrap = document.querySelector('.nav-wrap')
+let navBtns = document.querySelectorAll('.btn');
+let tabsWrap = document.querySelector('.tabs-wrap');
+let tabs = document.querySelectorAll('.tab');
+
+function addEvent () {
+    for (const navBtn of navBtns) {
+        navBtn.addEventListener('click',test);
+    }
+}
+const test = (e) => {
+    let btn = e.target
+    let target = btn.dataset.target;
+    tabs.forEach (tab => tab.style.display ="none")
+    navBtns.forEach (navBtn => navBtn.classList.remove('active'))
+    document.getElementById(target).style.display = "block";
+    document.querySelector(`[data-target=${target}]`).classList.add('active')
+}
+
+window.onload = addEvent();
